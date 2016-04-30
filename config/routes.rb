0e2fe8b3b767347dcb1d129255dae071e40ccb6f
Nhat Nguyen <hangnhat57@gmail.com>
2016-04-30 11:50:54 +0700

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   #patch 'show/:id' => 'posts#update'
   #delete 'show/:id' => 'posts#destroy'
   root 'posts#home'
+  get '/home' => 'posts#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
